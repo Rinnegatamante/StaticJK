47b2394f445a3a51c63ecfb91cd9182f7aa3f7ce
Rinnegatamante <rinnegatamante@gmail.com>
2025-07-28 00:30:44 +0200

    Disable fog rendering.

--- src/code/rd-vanilla/tr_init.cpp
+++ src/code/rd-vanilla/tr_init.cpp
@@ -1647,10 +1647,14 @@ void R_Register( void )
 
 	r_nocurves = ri.Cvar_Get ("r_nocurves", "0", CVAR_CHEAT );
 	r_drawworld = ri.Cvar_Get ("r_drawworld", "1", CVAR_CHEAT );
+#ifndef __vita__
 #ifdef JK2_MODE
 	r_drawfog = ri.Cvar_Get ("r_drawfog", "1", CVAR_CHEAT );
 #else
 	r_drawfog = ri.Cvar_Get ("r_drawfog", "2", CVAR_CHEAT );
+#endif
+#else
+	r_drawfog = ri.Cvar_Get ("r_drawfog", "0", CVAR_CHEAT );	
 #endif
 	r_lightmap = ri.Cvar_Get ("r_lightmap", "0", CVAR_CHEAT );
 	r_portalOnly = ri.Cvar_Get ("r_portalOnly", "0", CVAR_CHEAT );
